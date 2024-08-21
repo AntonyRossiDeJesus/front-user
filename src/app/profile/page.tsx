a123d5d10174974505fbf8d3337f52d6fc93563b
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-08-21 17:52:37 -0300

    deploy home

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import api from "../api/axios";
 import LogoutButton from "./LogoutButton";
 
 interface UserProfile {
@@ -14,52 +15,47 @@ const ProfilePage = () => {
   const [profile, setProfile] = useState<UserProfile | null>(null);
   const router = useRouter();
 
-  useEffect(() => {
-    // const fetchProfile = async () => {
-    //   try {
-    //     const response = await fetch(
-    //       "https://user-api-ivory.vercel.app/api/profile",
-    //       {
-    //         method: "GET",
-    //         credentials: "include",
-    //       }
-    //     );
+  // useEffect(() => {
+  // const fetchProfile = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://user-api-ivory.vercel.app/api/profile",
+  //       {
+  //         method: "GET",
+  //         credentials: "include",
+  //       }
+  //     );
 
-    //     if (response.ok) {
-    //       const data = await response.json();
-    //       setProfile(data.user);
-    //     } else {
-    //       router.push("/login");
-    //     }
-    //   } catch (error) {
-    //     console.error("Failed to fetch profile:", error);
-    //     router.push("/login");
-    //   }
-    // };
-    const fetchProfile = async () => {
-      try {
-        console.log("Fetching profile...");
-        const response = await fetch(
-          "https://user-api-ivory.vercel.app/api/profile",
-          {
-            method: "GET",
-          }
-        );
-        if (response.ok) {
-          console.log("Response status:", response.status);
-          const data = await response.json();
-          console.log("Profile data:", data);
-          setProfile(data.user);
-        } else {
-          console.log("Response error:", response.status);
-          router.push("/login");
-        }
-      } catch (error) {
-        console.error("Failed to fetch profile:", error);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setProfile(data.user);
+  //     } else {
+  //       router.push("/login");
+  //     }
+  //   } catch (error) {
+  //     console.error("Failed to fetch profile:", error);
+  //     router.push("/login");
+  //   }
+  // };
+  const fetchProfile = async () => {
+    try {
+      console.log("Fetching profile...");
+      const response = await api.get("/profile");
+
+      if (response.status === 200) {
+        console.log("Profile data:", response.data);
+        setProfile(response.data.user);
+      } else {
+        console.log("Response error:", response.status);
         router.push("/login");
       }
-    };
+    } catch (error) {
+      console.error("Failed to fetch profile:", error);
+      router.push("/login");
+    }
+  };
 
+  useEffect(() => {
     fetchProfile();
   }, [router]);
 
